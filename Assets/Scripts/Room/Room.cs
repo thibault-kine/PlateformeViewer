6123ce40ba556b42045d600f50451a4980d1436d
thibault-kine <thibault.kine@laplateforme.io>
2026-03-10 21:54:34 +0100

    First test
    I expect the script to set the room's data according to the mock files in Resources/MockData/ (it will do for now) at runtime only (i'll change it later through an Editor Script).

---
+++ Assets/Scripts/Room/Room.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+[Serializable]
+public class Room
+{
+    public string id;
+
+    public string name;
+    public string email;
+    public string type;
+
+    public int capacity;
+    public string building;
+    public string floor;
+    public string category;
+
+    public List<string> features;
+
+    public string generatedResourceName;
+
+
+    public static Room FromJson(string json)
+    {
+        return JsonUtility.FromJson<Room>(json);
+    }
+
+    public string ToJson()
+    {
+        return JsonUtility.ToJson(this, true);
+    }
+}
+
+[Serializable]
+public class RoomsFile
+{
+    public string exportDate;
+    public int totalRooms;
+    public List<Room> rooms;
+
+
+    public Room GetByID(string id)
+    {
+        return rooms.Find(room => room.id == id);
+    }
+
+    public Room GetByName(string name)
+    {
+        return rooms.Find(room => room.name == name);
+    }
+
+
+    public static RoomsFile FromJson(string json)
+    {
+        return JsonUtility.FromJson<RoomsFile>(json);
+    }
+
+    public string ToJson()
+    {
+        return JsonUtility.ToJson(this, true);
+    }
+}
---
+++ Assets/Scripts/Room/RoomLoader.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class RoomLoader : MonoBehaviour
+{
+    [SerializeField] GameObject buildingManager;
+    Dictionary<RoomApiResponse, Transform> dict = new();
+
+    private void Start()
+    {
+        List<Transform> children = new();
+        List<RoomApiResponse> responses = new();
+
+        // Get all the children of the building
+        foreach (Transform child in buildingManager.transform) 
+            children.Add(child);
+
+        // Get all the data in MockData/ and parse them into a RoomApiResponse object
+        var allMock = Resources.LoadAll("MockData");
+        foreach (var mock in allMock)
+        {
+            RoomApiResponse res;
+            DataLoader.LoadJson($"MockData/{mock.name}", out res);
+            responses.Add(res);
+        }
+
+        // Cycle through children and set them up accordingly if their ID correspond
+        foreach (var child in children)
+        {
+            foreach (var res in responses)
+            {
+                if (child.name != res.room.name)
+                    continue;
+
+                var _bc = child.AddComponent<BoxCollider>();
+                _bc.size = child.GetComponent<MeshRenderer>().bounds.size;
+
+                var _rsi = child.AddComponent<RoomStatusIndicator>();
+                _rsi.SetRenderer(child.GetComponent<MeshRenderer>());
+                _rsi.SetMaterialIndex(0);
+                _rsi.SetVisualConfig(Resources.Load<VisualConfig>("Config/VisualConfig_"));
+
+                var _rc = child.AddComponent<RoomController>();
+                _rc.roomData = Resources.Load<RoomDataSO>($"RoomData/Room_{res.room.name}");
+                _rc.SetStatusIndicator(child.GetComponent<RoomStatusIndicator>());
+
+                Debug.Log($"Setup for {res.room.name} done!");
+                dict.Add(res, child);
+            }
+        }
+    }
+}
